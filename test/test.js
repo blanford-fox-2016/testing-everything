ecfe092ebdc355704eb0187f38fc22a261be1932
Aji Lantang Mardika <email": null>
2016-11-07 13:02:34 +0700

    testing

---
+++ test/test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const mocha = require('mocha');
+let chai = require('chai');
+let assert = chai.assert;
+let expect = chai.expect;
+const test = require('./index');
+
+
+//===============
+// testing ======
+//===============
+
+describe('#isEven()',()  => {
+  it('it should return true if input even number',() => {
+    assert.equal(true,test.isEven(2));
+  })
+})
+
+describe('#isOdd()',() => {
+  it('it should return true if input odd number',() => {
+    assert.equal(true,test.isOdd(3))
+  })
+})
+
+//===============
+// curry test ===
+//===============
+expect(test.calculate(2)(6)).to.equal(8);
+expect(test.calculate(2)(2)).to.equal(4);
+expect(test.calculate(2)(6)).to.equal(8);
+expect(test.calculate(10)(2)).to.equal(10); //assertion error because calculate return 12
