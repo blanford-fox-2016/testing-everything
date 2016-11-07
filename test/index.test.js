d7149be77ee5518798c3e9d6762bdd1e26b8308a
Ken Duigraha Putra <email": null>
2016-11-07 11:17:26 +0700

    testing driven development

--- test/index.test.js
+++ test/index.test.js
@@ -9,25 +9,32 @@ const should = chai.should()
 /*
   * require file test
 */
-// const Test = require('../controller/controller.index.js')
+const Test = require('../controller/controller.index.js')
 
 /*
   * test the function multiple
   * result must be the multiple value of params a & b
 */
 describe('Testing Multiple Function', () => {
-  it('it should return the multiple value of params a & b', () => {
+  it('it should return the multiple value of params a & b (BOTH NUMBER)', () => {
     // assert.equal(12, Test.multiple(3, 4))
     expect(Test.multiple(3, 4)).to.equal(12)
   })
-})
 
-/*
-  * test the function isArray
-  * result must be the type of params
-*/
-describe('Testing isArray Function', () => {
-  it('it should return the type of params', () => {
-    expect(Test.isArray([3, 4])).be.a('object')
+  it('it should return the multiple value of params a & b (BOTH STRING)', () => {
+    expect(Test.multiple('3','4')).to.equal(12)
   })
+
+  it('it should return the value of function multiple (BOTH EMPTY)', () => {
+    assert.strictEqual(Test.multiple(), 'no params')
+  })
+
+  it('it should return the value of function multiple (ONLY 1 PARAM) ', () => {
+    expect(Test.multiple(3)).to.equal('only 1 param')
+  })
+
+  it('it should return the value of function multiple (BOTH CHAR)', () => {
+    expect(isNaN(Test.multiple('a', 'b'))).to.equal(true)
+  })
+
 })
