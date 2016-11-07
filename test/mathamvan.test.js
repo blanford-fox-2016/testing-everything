9b84f8c05444cbd19e10c1196e63f797b215566a
Septian A Tama <sfirst.tama@gmail.com>
2016-11-07 11:53:12 +0700

    using beforeEach hooks

--- test/mathamvan.test.js
+++ test/mathamvan.test.js
@@ -7,17 +7,24 @@ const mathamvan = require('../mathamvan');
 
 const add = mathamvan.add;
 
-describe('add', () => {
+describe('add function will return the sum result of both parameter', () => {
+
+  let a;
+
+  beforeEach(() => {
+    a = 1;
+  });
+
   it('should sum first parameter and second parameter', () => {
-    expect(add(1,2)).eql(3);
+    expect(add(a,2)).eql(3);
   });
   it('should give message that add function need 2 number as parameters', () => {
-    expect(add('a', 1)).eql('Parameters are not valid, both must be a number');
+    expect(add('a', a)).eql('Parameters are not valid, both must be a number');
   });
   it('should give message that the function need 2 parameters', () => {
     expect(add()).eql('It needs two parameters');
   });
   it('should give message that the function need 2 parameters', () => {
-    expect(add(1)).eql('It needs two parameters');
+    expect(add(a)).eql('It needs two parameters');
   });
 })
