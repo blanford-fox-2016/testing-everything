9faaaeaa84c459a55de84efe047b5971fde8a349
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-07 11:03:26 +0700

    testing input string must empty works

---
+++ controller/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    isEmpty: function (input) {
+        if (input.length == 0) return true
+        else return false
+    }
+}
--- test/index.js
+++ test/index.js
@@ -1,33 +1,34 @@
 const mocha = require('mocha')
-const expect = mocha.expect
+const expect = require('chai').expect
 const controller = require('../controller/index')
 
 describe("Test if input string empty or not", function () {
     it("Return true if input string is empty", function () {
-        expect(controller.isEmpty().to.be.true)
+        expect(controller.isEmpty("")).to.be.true
+        expect(controller.isEmpty("adsa")).to.be.false
     })
 })
 
 describe("Test if input more than four char", function () {
     it("Return true if input string is more than four", function () {
-        expect(controller.moreThanFour("abcde").to.be.true)
+        expect(controller.moreThanFour("abcde")).to.be.true
     })
 })
 
 describe("Test is input only contains number", function () {
     it("Return true if input string is only contains number", function () {
-        expect(controller.onlyNumber("123").to.be.true)
+        expect(controller.onlyNumber("123")).to.be.true
     })
 })
 
 describe("Test is input like email format", function () {
     it("Return true if input like email format", function () {
-        expect(controller.isEmail("dharmadi93@gmail.com").to.be.true)
+        expect(controller.isEmail("dharmadi93@gmail.com")).to.be.true
     })
 })
 
 describe("Test is input contains one symbol, capital letter, and number", function () {
     it("Return true if input contains one symbol, capital letter, and number", function () {
-        expect(controller.hardPassword("@Abcde1").to.be.true)
+        expect(controller.hardPassword("@Abcde1")).to.be.true
     })
 })
