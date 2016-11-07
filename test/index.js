cc86ec32e8a74440d07102d7950f0e8527538b2f
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-07 10:56:40 +0700

    add TDD

--- test/index.js
+++ test/index.js
@@ -1,4 +1,33 @@
 const mocha = require('mocha')
 const expect = mocha.expect
-const controller = require('../controller')
 
+
+describe("Test if input string empty or not", function () {
+    it("Return true if input string is empty", function () {
+        expect(controller.isEmpty().to.be.true)
+    })
+})
+
+describe("Test if input more than four char", function () {
+    it("Return true if input string is more than four", function () {
+        expect(controller.moreThanFour("abcde").to.be.true)
+    })
+})
+
+describe("Test is input only contains number", function () {
+    it("Return true if input string is only contains number", function () {
+        expect(controller.onlyNumber("123").to.be.true)
+    })
+})
+
+describe("Test is input like email format", function () {
+    it("Return true if input like email format", function () {
+        expect(controller.isEmail("dharmadi93@gmail.com").to.be.true)
+    })
+})
+
+describe("Test is input contains one symbol, capital letter, and number", function () {
+    it("Return true if input contains one symbol, capital letter, and number", function () {
+        expect(controller.hardPassword("@Abcde1").to.be.true)
+    })
+})
