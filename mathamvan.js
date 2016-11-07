2011dcc982f27b2bf27cb464ec486f64ac96d127
Septian A Tama <sfirst.tama@gmail.com>
2016-11-07 11:15:36 +0700

    validation when one of the input isNaN works

--- mathamvan.js
+++ mathamvan.js
@@ -1,7 +1,11 @@
 'use strict'
 
 let add = (a, b) => {
+  if (isNaN(a) === true || isNaN(b) === true) {
+    return 'Parameters are not valid, both must be a number';
+  } else{
     return a + b;
+  }
 }
 
 module.exports = {
