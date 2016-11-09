b71de1392909d7f068d311ef017bfaea5c38a47c
ryan <ryan.andhaka@gmail.com>
2016-11-09 22:12:12 +0700

    all testing, success

--- controllers/controller.index.js
+++ controllers/controller.index.js
@@ -1,1 +1,17 @@
+'use strict'
 
+  let subtraction = (x, y) => {
+    if(typeof x === 'undefined' && typeof y === 'undefined'){
+      // variables x & y are not exist
+      return 'no parameter assigned on both variables'
+    }else if(x && !y || y && !x){
+      // only 1 variable exist
+      return 'only 1 variable'
+    }else{
+      return x - y
+    }
+  }
+
+  module.exports = {
+    subtraction: subtraction
+  }
